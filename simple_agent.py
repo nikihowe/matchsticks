4e8f0826ee836afcfa1de1cedbc44f64ce109ee2
Niki Howe <nikihowe@gmail.com>
2021-05-11 11:49:33 -0400

    Make agent work on game where reward only occurs at the end

--- simple_agent.py
+++ simple_agent.py
@@ -60,7 +60,7 @@ class Agent(object):
     # If we've never seen this position, initialize it into the Q table
     if game.word not in self.Q:
       possible_moves = get_possible_moves()
-      moves_and_values = list(map((lambda x: (x, 3.)), possible_moves))
+      moves_and_values = list(map((lambda x: (x, 0.)), possible_moves))
       self.Q[game.word] = dict(moves_and_values)
 
     # Choose a move according to the policy
@@ -75,21 +75,58 @@ class Agent(object):
     self.rewards.append(reward)
 
   def update(self) -> None:
+    discount = 0.9
+    current_return = 0.
+    # print("updating")
+    # print("the history is", self.history)
+    # print("the rewards are", self.rewards)
     for i, (word, move) in reversed(list(enumerate(self.history))):
-      self.Q[word][move] = self.Q[word][move] * 0.9 + 0.1 * self.rewards[i]
 
-# TODO: move from a reward-per-move setting to a all-rewards-after-episode setting
+      current_return = discount * current_return + self.rewards[i]
+
+      # Check whether this state-action pair happened earlier
+      if (word, move) in self.history[:i]:
+        continue
+
+      # Running average
+      self.Q[word][move] = self.Q[word][move] * 0.9 + 0.1 * current_return
+
+  def end_episode(self) -> None:
+    self.history = []
+    self.rewards = []
+
+
+# Now our setting is one where the rewards are summed together and only given at the end of the episode
 if __name__ == "__main__":
   agent = Agent()
-  episode_length = 10
-  num_episodes = 30
-  for _ in range(num_episodes):
-    print("Q is", agent.Q)
-    g1 = Game()
-    for _ in range(episode_length):
+  episode_length = 5
+  num_episodes = 5000
+  np.random.seed(0)
+  all_final_rewards = []
+  randomness = np.random.choice(names, num_episodes * episode_length)
+  for j in range(num_episodes):
+    # print("Q is", agent.Q)
+    g1 = Game(randomness[j*episode_length])
+    episode_reward = 0
+    for i in range(episode_length):
+      if i:
+        agent.receive_reward(0)  # Always 0 except at the end, when agent gets the true amounts all smooshed together
+        g1.word = randomness[j+i]
       m = agent.move(g1)
       r = g1.move(m)
-      agent.receive_reward(r)
+      episode_reward += r
+    agent.receive_reward(episode_reward)
+    all_final_rewards.append(episode_reward)
     agent.update()
+    agent.eps *= 0.999
+    agent.end_episode()
 
-  print(agent.Q)
+  import matplotlib.pyplot as plt
+  from scipy.signal import savgol_filter
+
+  smoothed_rewards = savgol_filter(all_final_rewards, 101, 3)  # window size 51, polynomial order 3
+
+  plt.plot(smoothed_rewards, 'o')
+  plt.show()
+
+
