d3167bb81a548f74e343726d3aa51d6348f0f39e
Niki Howe <nikihowe@gmail.com>
2021-05-11 20:57:36 -0400

    Add monte-carlo RL learning player, a pretrained player which loads in Q table, and an interface to play as a human

--- arena.py
+++ arena.py
@@ -1,6 +1,6 @@
 # (c) Nikolaus Howe
 
-from player import Player, TrivialPlayer
+from player import Player, TrivialPlayer, RandomPlayer, MCPlayer, PretrainedPlayer, HumanPlayer
 from game import Game
 
 
@@ -21,9 +21,10 @@ class Arena(object):
 
   def play(self):
     game_on = True
+    move_counter = 0
     while game_on:
       if self.verbose:
-        print("Game state:", self.game.layers)
+        print("Game state:", self.game.get_state())
 
       # Get the active player to choose a move
       move = self.next_player_to_move.move(self.game)
@@ -33,26 +34,70 @@ class Arena(object):
       # Tell game to do the move
       game_on = self.game.play_move(move)
 
+      # If it's a learning player, give it a reward for this move (but not on its first move)
+      if isinstance(self.next_player_to_move, MCPlayer) and move_counter >= 2:
+        self.next_player_to_move.receive_reward(0.)
+
       # Change the active player
       self.switch_active_player()
+
+      # Increment the move counter
+      move_counter += 1
 
     if self.verbose:
       print("Game over!")
 
     # Tell the players whether they won or lost
-    self.next_player_to_move.you_win()
+    self.next_player_to_move.receive_reward(1.)
     if self.verbose:
       print(f"{self.next_player_to_move.name} won!")
     self.switch_active_player()
-    self.next_player_to_move.you_lose()
+    self.next_player_to_move.receive_reward(-1.)
     if self.verbose:
       print(f"{self.next_player_to_move.name} lost!")
 
 
 if __name__ == "__main__":
-  g1 = Game()
-  p1 = TrivialPlayer("Alice")
-  p2 = TrivialPlayer("Bob")
-  a1 = Arena(g1, p1, p2, verbose=True)
+  # p1 = MCPlayer("Alice")
+  # p2 = MCPlayer("Bob")
+  # for i in range(100_000):
+  #   if i and i % 10_000 == 0:
+  #     print(i)
+  #   # Player 1 goes first
+  #   g1 = Game()
+  #   a1 = Arena(g1, p1, p2)
+  #   a1.play()
+  #   p1.update()
+  #   p1.end_episode()
+  #   p2.update()
+  #   p2.end_episode()
+  #
+  #   # Player 2 goes first
+  #   g1 = Game()
+  #   a1 = Arena(g1, p2, p1)
+  #   a1.play()
+  #   p1.update()
+  #   p1.end_episode()
+  #   p2.update()
+  #   p2.end_episode()
 
+  p1 = PretrainedPlayer('Alice')
+  p2 = HumanPlayer('Niki')
+
+  print(p1.Q)
+  # print(p2.Q)
+
+  # Make the players play deterministically (greedily)
+  p1.eps = 0
+  # p2.eps = 0
+
+  g1 = Game()
+  a1 = Arena(g1, p1, p2, verbose=True)
   a1.play()
+
+  g1 = Game()
+  a1 = Arena(g1, p2, p1, verbose=True)
+  a1.play()
+
+  # p1.save_q(p1.name)
+  # p2.save_q(p2.name)
--- player.py
+++ player.py
@@ -1,9 +1,12 @@
 # (c) Nikolaus Howe 2021
 
 import numpy as np
+import pickle as pkl
 import random
 from abc import ABC, abstractmethod
 from overrides import overrides
+
+from game import Game
 
 
 class Player(ABC):
@@ -11,11 +14,11 @@ class Player(ABC):
   An abstract class for defining players
   """
 
-  def __init__(self, name="Alice"):
+  def __init__(self, name: str = "Alice"):
     self.name = name
 
   @abstractmethod
-  def move(self, game):
+  def move(self, game: Game):
     """
     Get the player to play a move among allowed_moves
 
@@ -24,19 +27,17 @@ class Player(ABC):
     """
     pass
 
-  def you_win(self):
-    """
-    Tell the player that they won.
-    :return:
-    """
+  def receive_reward(self, reward: float):
     pass
 
-  def you_lose(self):
-    """
-    Tell the player that they lost.
-    :return:
-    """
+  def update(self):
     pass
+
+  def end_episode(self):
+    pass
+
+  def save_q(self, filename: str):
+    print("Saving has not been implemented for this player")
 
 
 class TrivialPlayer(Player):
@@ -45,7 +46,7 @@ class TrivialPlayer(Player):
   """
 
   @overrides
-  def move(self, game):
+  def move(self, game: Game):
     return 1, 1, 1
 
 
@@ -55,7 +56,7 @@ class RandomPlayer(Player):
   """
 
   @overrides
-  def move(self, game):
+  def move(self, game: Game):
     return random.choice(game.get_allowed())
 
 
@@ -63,31 +64,134 @@ class MCPlayer(Player):
   """
   An on-policy first-visit MC control player.
   """
-  #
-  # @overrides
-  # def __init__(self) -> None:
-  #   super().__init__()
-  #   self.Q = {}
-  #   self.rewards = []
-  #   self.eps = 0.05
-  #   self.history = []
-  #
-  # def policy(self, game_word) -> int:
-  #   # Choose randomly self.eps of the time
-  #   if np.random.uniform() < self.eps:
-  #     return np.random.choice(get_possible_moves())
-  #   else:
-  #     q_row = list(self.Q[game_word].items())  # Turn the dict into a list
-  #     q_row.sort(key=lambda x: x[1], reverse=True)  # Sort in descending order
-  #     # print("sorted row is", q_row)
-  #     return q_row[0][0]  # return the move with the highest Q value
-  #
-  # @overrides
-  # def move(self, game):
-  #   # Check if we have seen this state before
-  #   # If we haven't initialize the Q table for that state
-  #   if not game.levels in self.Q:
-  #     self.Q[game.levels.copy()] =
-  #
-  #
-  #   return 1, 1, 1
+
+  @overrides
+  def __init__(self, name='Alice') -> None:
+    super().__init__(name=name)
+    self.Q = {}
+    self.rewards = []
+    self.eps = 0.05
+    self.history = []
+
+  def policy(self, game: Game) -> int:
+    # Choose randomly self.eps of the time
+    if np.random.uniform() < self.eps:
+      return random.choice(game.get_allowed())
+    else:
+      q_row = list(self.Q[game.get_state()].items())  # Turn the dict into a list
+      q_row.sort(key=lambda x: x[1], reverse=True)  # Sort in descending order
+      # print("sorted row is", q_row)
+      return q_row[0][0]  # return the move with the highest Q value
+
+  @overrides
+  def move(self, game: Game) -> int:
+    # If we've never seen this position, initialize it into the Q table
+    game_state = game.get_state()
+    if game_state not in self.Q:
+      possible_moves = game.get_allowed()
+      moves_and_values = list(map((lambda x: (x, 0.)), possible_moves))
+      # print("setting moves and values", moves_and_values)
+      self.Q[game_state] = dict(moves_and_values)
+
+    # Choose a move according to the policy
+    move = self.policy(game)
+
+    # Record state and move in history
+    self.history.append((game_state, move))
+
+    return move
+
+  def receive_reward(self, reward: float) -> None:
+    self.rewards.append(reward)
+
+  def update(self) -> None:
+    discount = 0.9
+    current_return = 0.
+    # print("updating")
+    # print("the history is", self.history)
+    # print("the rewards are", self.rewards)
+    for i, (word, move) in reversed(list(enumerate(self.history))):
+
+      current_return = discount * current_return + self.rewards[i]
+
+      # Check whether this state-action pair happened earlier
+      if (word, move) in self.history[:i]:
+        continue
+
+      # Running average
+      self.Q[word][move] = self.Q[word][move] * 0.9 + 0.1 * current_return
+
+  def end_episode(self) -> None:
+    self.history = []
+    self.rewards = []
+
+  def save_q(self, filename) -> None:
+    with open(filename, 'wb') as f:
+      pkl.dump(self.Q, f)
+
+    print(f"Saved Q table as '{filename}'")
+
+
+class PretrainedPlayer(Player):
+  """
+  A class which loads in a Q table
+  """
+
+  @overrides
+  def __init__(self, q_filename, name=None):
+    del name
+    super().__init__(name=q_filename)
+    try:
+      with open(q_filename, 'rb') as f:
+        self.Q = pkl.load(f)
+    except Exception as e:
+      print(f"Couldn't load file {q_filename}.")
+      print("The exception was", e)
+
+  @overrides
+  def move(self, game: Game) -> int:
+    # If we've never seen this position, initialize it into the Q table
+    game_state = game.get_state()
+    if game_state not in self.Q:
+      possible_moves = game.get_allowed()
+      moves_and_values = list(map((lambda x: (x, 0.)), possible_moves))
+      # print("setting moves and values", moves_and_values)
+      self.Q[game_state] = dict(moves_and_values)
+
+    # Choose a move according to the policy
+    move = self.policy(game)
+
+    return move
+
+  def policy(self, game: Game) -> int:
+    q_row = list(self.Q[game.get_state()].items())  # Turn the dict into a list
+    q_row.sort(key=lambda x: x[1], reverse=True)  # Sort in descending order
+    # print("sorted row is", q_row)
+    return q_row[0][0]  # return the move with the highest Q value
+
+
+class HumanPlayer(Player):
+
+  @overrides
+  def __init__(self, name='Human'):
+    super().__init__(name=name)
+
+  @overrides
+  def move(self, game: Game):
+    move_str = input("Input your move in the form (layer, low, high).\n")
+    try:
+      move = tuple(map(int, move_str[1:-1].split(', ')))
+    except Exception as e:
+      print("Couldn't read your move.")
+      print("The exception was", e)
+      raise SystemExit
+
+    assert (
+      len(move) == 3
+      and isinstance(move[0], int)
+      and isinstance(move[1], int)
+      and isinstance(move[2], int)
+      and game.is_allowed(move)
+    )
+
+    return move
